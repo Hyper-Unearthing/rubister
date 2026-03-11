fc751806f346bc81a8dec5f2bcbf627019df4b80
billybonks <sebastienstettler@gmail.com>
2026-03-11 13:59:11 +0800

    compaction improvement

--- lib/sessions/concerns/basic_compaction.rb
+++ lib/sessions/concerns/basic_compaction.rb
@@ -1,18 +1,7 @@
 module BasicCompaction
   def compaction(adapter, messages_to_keep: 2)
-    all_messages = message_entries
-    return nil if all_messages.length <= messages_to_keep
-
-    last_compaction = last_compaction_entry
-    start_index = 0
-    # for now we always summarize from the beginning to ensure the full context is integrated
-    # even if we have a previous summary.
-
-    split_index = all_messages.length - messages_to_keep
-    return nil if start_index >= split_index
-
-    entries_to_summarise = all_messages[start_index...split_index]
-    first_kept_entry = all_messages[split_index]
+    entries_to_summarise = compaction_source_messages
+    return nil if entries_to_summarise.empty?
 
     result = CompactionPrompt.new(adapter, entries_to_summarise, last_summary: last_summary).post
     text_parts = result[:choices]&.dig(0, :content).select { |part| part[:type] == 'text' }
