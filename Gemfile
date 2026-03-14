f481e209b6f4b85ecd83355464082f3377519b9e
billybonks <sebastienstettler@gmail.com>
2026-03-14 13:13:55 +0800

    feat: db management tools

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '4.0.1'
 gem 'base64'
 gem 'llm_gateway', path: '../llm_gateway'
 gem 'singleton'
+gem 'activerecord'
+gem 'sqlite3'
 
 group :development do
   gem 'debug'
---
+++ lib/database_config.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'active_record'
+require_relative 'instance_file_scope'
+
+module DatabaseConfig
+  module_function
+
+  def db_path
+    InstanceFileScope.path('gruv.sqlite3')
+  end
+
+  def establish_connection!
+    ActiveRecord::Base.establish_connection(
+      adapter: 'sqlite3',
+      database: db_path,
+      timeout: 5000
+    )
+  end
+end
